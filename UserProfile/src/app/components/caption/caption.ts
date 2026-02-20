f0eda03200f7b5993f9edd30fa11f2256770dacc
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-02-20 18:21:41 +0100

    feat(caption): templating Caption component for use in Home component.

--- UserProfile/src/app/components/caption/caption.ts
+++ UserProfile/src/app/components/caption/caption.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-caption',
-  imports: [],
+  imports: [ RouterLink],
   templateUrl: './caption.html',
   styleUrl: './caption.css',
 })
--- UserProfile/src/app/components/home/home.ts
+++ UserProfile/src/app/components/home/home.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Service } from '../../services/service';
+import { Observable } from 'rxjs';
+import { Caption } from '../caption/caption';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, Caption],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
+  private clientHttp = inject(Service);
 }
